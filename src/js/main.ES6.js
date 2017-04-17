a457b2163b0b13595bb4a86bb306269fc68a5350
Pavel Ledovskikh <o_gredi@mail.ru>
2017-04-17 20:10:57 +0300

    закончил главный баннер

--- src/js/main.ES6.js
+++ src/js/main.ES6.js
@@ -1,6 +1,11 @@
 /**
  * Created by Павел on 07.04.2017.
  */
+function checkNumber(number) {
+    if (number > 4) number = 0;
+    if (number < 0) number = 4;
+    return number;
+}
 $(document).ready(() => {
     let $fancybox = $(".fancybox"), $fancyClose = $(".fancybox__close"),
         $fancyImg = $(".fancybox img");
@@ -9,9 +14,9 @@ $(document).ready(() => {
     });
     $(".works-item__button").click(function () {
         let imgHeight = $(this).parent().siblings("img").height();
-        $fancyImg.attr("src",$(this).parent().siblings("img").attr("src"));
+        $fancyImg.attr("src", $(this).parent().siblings("img").attr("src"));
         $fancyImg.css({
-            marginTop: (($(window).height() - imgHeight)/2)
+            marginTop: (($(window).height() - imgHeight) / 2)
         });
         console.log(imgHeight);
         $fancybox.css({
@@ -19,7 +24,27 @@ $(document).ready(() => {
             height: $(window).height()
         }).fadeIn(500);
     });
-    $fancybox.click(()=>{
+    $fancybox.click(() => {
         $fancybox.css({display: "none"});
+    });
+
+    //Слайдер для main-banner
+    let $bannerItems = $(".main-banner__item");
+
+    $(".main-banner__controls-next").click(() => {
+        let current = $(".main-banner__item_active").index();
+        $bannerItems.eq(current).removeClass("main-banner__item_active");
+        $(".main-banner__item_next").removeClass("main-banner__item_next").addClass("main-banner__item_active");
+        $(".main-banner__item_prev").removeClass("main-banner__item_prev");
+        $bannerItems.eq(checkNumber(checkNumber(current + 1) + 1)).addClass("main-banner__item_next");
+        $bannerItems.eq(current).addClass("main-banner__item_prev");
+    });
+    $(".main-banner__controls-prev").click(() => {
+        let current = $(".main-banner__item_active").index();
+        $bannerItems.eq(current).removeClass("main-banner__item_active");
+        $(".main-banner__item_prev").removeClass("main-banner__item_prev").addClass("main-banner__item_active");
+        $(".main-banner__item_next").removeClass("main-banner__item_next");
+        $bannerItems.eq(checkNumber(checkNumber(current - 1) - 1)).addClass("main-banner__item_prev");
+        $bannerItems.eq(current).addClass("main-banner__item_next");
     });
 });
--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,11 @@
 /**
  * Created by Павел on 07.04.2017.
  */
+function checkNumber(number) {
+    if (number > 4) number = 0;
+    if (number < 0) number = 4;
+    return number;
+}
 $(document).ready(function () {
     var $fancybox = $(".fancybox"),
         $fancyClose = $(".fancybox__close"),
@@ -24,5 +29,25 @@ $(document).ready(function () {
     });
     $fancybox.click(function () {
         $fancybox.css({ display: "none" });
+    });
+
+    //Слайдер для main-banner
+    var $bannerItems = $(".main-banner__item");
+
+    $(".main-banner__controls-next").click(function () {
+        var current = $(".main-banner__item_active").index();
+        $bannerItems.eq(current).removeClass("main-banner__item_active");
+        $(".main-banner__item_next").removeClass("main-banner__item_next").addClass("main-banner__item_active");
+        $(".main-banner__item_prev").removeClass("main-banner__item_prev");
+        $bannerItems.eq(checkNumber(checkNumber(current + 1) + 1)).addClass("main-banner__item_next");
+        $bannerItems.eq(current).addClass("main-banner__item_prev");
+    });
+    $(".main-banner__controls-prev").click(function () {
+        var current = $(".main-banner__item_active").index();
+        $bannerItems.eq(current).removeClass("main-banner__item_active");
+        $(".main-banner__item_prev").removeClass("main-banner__item_prev").addClass("main-banner__item_active");
+        $(".main-banner__item_next").removeClass("main-banner__item_next");
+        $bannerItems.eq(checkNumber(checkNumber(current - 1) - 1)).addClass("main-banner__item_prev");
+        $bannerItems.eq(current).addClass("main-banner__item_next");
     });
 });
